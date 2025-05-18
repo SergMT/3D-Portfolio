ca467fb93b4186c9da9b5b04506fd714c523cd86
Sergio <bloginserg@gmail.com>
2025-05-17 22:24:17 -0600

    tailwind css styles work as expected

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl underline">Serg's 3D Portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
